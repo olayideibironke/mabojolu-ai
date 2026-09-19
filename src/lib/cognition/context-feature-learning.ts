4f62f74d024365154aaefb05c2467122438fa774
Olayide Ibironke <ibironkeolayide@gmail.com>
2026-09-19 14:08:53 -0400

    Let context learner evaluate nuisance history dimensions

--- src/lib/cognition/context-feature-learning.ts
+++ src/lib/cognition/context-feature-learning.ts
@@ -63,6 +63,8 @@ const FEATURE_NAMES:
     "candidateMatchesLastAction",
     "candidateMatchesLastProductiveAction",
     "candidateNoEffectAttempts",
+    "distinctActionsSeen",
+    "historyLength",
     "lastChangeArity",
     "lastValueShapes",
     "stepsSinceCandidateAttempt",
